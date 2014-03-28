2f84c289c103d1410eacff276bbf04cb0bcc1b87
JorgeP86 <jorge.pareja86@gmail.com>
2014-03-28 22:34:20 +0100

    28/03/2014 - Añadido que extiende del repositorio propio

--- src/main/resources/com/prestafacturaService/mongo/repositories/UsuarioRepository.java
+++ src/main/resources/com/prestafacturaService/mongo/repositories/UsuarioRepository.java
@@ -2,10 +2,11 @@ package com.prestafacturaService.mongo.repositories;
 
 import org.springframework.stereotype.Repository;
 
+import com.pestafacturaService.mongo.repositories.propios.UsuarioRepositoryPropio;
 import com.prestafacturaService.mongo.dto.Usuario;
 
 
 @Repository
-public interface UsuarioRepository extends GenericRepository<Usuario>{
+public interface UsuarioRepository extends UsuarioRepositoryPropio, GenericRepository<Usuario>{
 
 }
