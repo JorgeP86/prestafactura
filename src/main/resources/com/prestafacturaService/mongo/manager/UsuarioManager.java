ed84cf3fab01be94f399948a30cdbbee6b948e08
JorgeP86 <jorge.pareja86@gmail.com>
2014-04-01 18:14:18 +0200

    01/04/2014 - Eliminado warnnings

--- src/main/resources/com/prestafacturaService/mongo/manager/UsuarioManager.java
+++ src/main/resources/com/prestafacturaService/mongo/manager/UsuarioManager.java
@@ -3,8 +3,6 @@ package com.prestafacturaService.mongo.manager;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import com.prestafacturaService.mongo.dto.Usuario;
