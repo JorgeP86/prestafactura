8ebd9d4df505c78eab2178f3c8cac1ef11f35cf0
JorgeP86 <jorge.pareja86@gmail.com>
2014-04-08 11:01:46 +0200

    08/04/2014 - Añadido bean para Custom Queries

--- target/classes/com/prestafacturaService/mongo/repositories/MonedaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prestafacturaService.mongo.repositories;
-
-import org.springframework.stereotype.Repository;
-
-import com.prestafacturaService.mongo.dto.Moneda;
-
-@Repository
-public interface MonedaRepository extends GenericRepository<Moneda>{
-}
