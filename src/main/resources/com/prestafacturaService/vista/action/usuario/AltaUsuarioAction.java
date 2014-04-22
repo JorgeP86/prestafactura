d7c1f1b3cb7ae5f49d6d84bab2a94ea738973a30
JorgeP86 <jorge.pareja86@gmail.com>
2014-04-22 23:07:22 +0200

    actionUsuario 22/04/2014

---
+++ src/main/resources/com/prestafacturaService/vista/action/usuario/AltaUsuarioAction.java
@@ -0,0 +1,176 @@
+package com.prestafacturaService.vista.action.usuario;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.prestafacturaService.mongo.dto.Rol;
+import com.prestafacturaService.mongo.dto.Usuario;
+import com.prestafacturaService.mongo.manager.RolManager;
+import com.prestafacturaService.mongo.manager.UsuarioManager;
+
+public class AltaUsuarioAction extends ActionSupport implements ServletRequestAware{
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2284495396765182580L;
+	
+	
+	public static final String INPUT = "input";
+	public static final String SUCCESS = "success";
+	
+	private String nombre;
+	private String apellido1;
+	private String apellido2;
+	private String login;
+	private String password;
+	private String email;
+	private String departamento;
+    private String nombreRol;
+    
+    private HttpServletRequest servletRequest;
+  
+
+	@Autowired
+    private RolManager rolManager;
+	@Autowired
+	private UsuarioManager usuarioManager;
+    
+    
+	public String execute(){
+		clearFieldErrors();
+	try{
+		Rol rolUsuario=rolManager.ObtenerRolByName(nombreRol);
+		Usuario usuarioNuevo=new Usuario();
+		usuarioNuevo.setNombre(nombre);
+		usuarioNuevo.setApellido1(apellido1);
+		usuarioNuevo.setApellido2(apellido2);
+		usuarioNuevo.setDepartamento(departamento);
+		usuarioNuevo.setEmail(email);
+		usuarioNuevo.setLogin(login);
+		usuarioNuevo.setPassword(password);
+		usuarioNuevo.setRol(rolUsuario);
+		usuarioNuevo.setFechaAlta(new Date());
+		
+			Usuario usuarioCreado=usuarioManager.saveUsuario(usuarioNuevo);
+			servletRequest.setAttribute("usuarioCreado", usuarioCreado);
+			return SUCCESS;
+		
+		} catch (Exception e){
+			addFieldError("invalidUsuarioAlta","El usuario no se ha creado correctamente");
+		}
+	return INPUT;
+}
+	
+	 public void validate() {
+		 if(nombre==null || nombre.trim().equals("")){
+			 addFieldError("nombreUsuarioAlta",getText("nombreUsuarioAlta.Invalid"));
+		 } 
+		 if(apellido1==null || apellido1.trim().equals("")){
+			 addFieldError("apellido1UsuarioAlta",getText("apellido1UsuarioAlta.Invalid"));
+		 } 
+		 if(apellido2==null || apellido2.trim().equals("")){
+			 addFieldError("apellido2UsuarioAlta",getText("apellido2UsuarioAlta.Invalid"));
+		 } 
+		 if(login==null || login.trim().equals("")){
+			 addFieldError("loginUsuarioAlta",getText("loginUsuarioAlta.Invalid"));
+		 }
+		 if(password==null || password.trim().equals("")){
+			 addFieldError("passwordUsuarioAlta",getText("passwordUsuarioAlta.Invalid"));
+		 } 
+		 if(email==null || email.trim().equals("")){
+			 addFieldError("emailUsuarioAlta",getText("emailUsuarioAlta.Invalid"));
+		 }
+		 if(departamento==null || departamento.trim().equals("")){
+			 addFieldError("departamentoUsuarioAlta",getText("departamentoUsuarioAlta.Invalid"));
+		 } 
+		 if(nombreRol==null || nombreRol.trim().equals("")){
+			 addFieldError("(nombreRolUsuarioAlta",getText("nombreRolUsuarioAlta.Invalid"));
+		 }
+		 
+	 }
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido1() {
+		return apellido1;
+	}
+
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+
+	public String getApellido2() {
+		return apellido2;
+	}
+
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+	public String getNombreRol() {
+		return nombreRol;
+	}
+
+	public void setNombreRol(String nombreRol) {
+		this.nombreRol = nombreRol;
+	}
+	
+	public HttpServletRequest getServletRequest() {
+		return servletRequest;
+	}
+
+	public void setServletRequest(HttpServletRequest servletRequest) {
+		this.servletRequest = servletRequest;
+	}
+
+
+	
+	
+	
+
+}
