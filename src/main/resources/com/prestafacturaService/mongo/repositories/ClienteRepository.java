6ea4020c61f4049e21b8cb56e3e52f3dcdbee881
JorgeP86 <jorge.pareja86@gmail.com>
2014-04-29 14:18:29 +0200

    29/04/2014 actualizado repository

--- src/main/resources/com/prestafacturaService/mongo/repositories/ClienteRepository.java
+++ src/main/resources/com/prestafacturaService/mongo/repositories/ClienteRepository.java
@@ -3,9 +3,10 @@ package com.prestafacturaService.mongo.repositories;
 import org.springframework.stereotype.Repository;
 
 import com.prestafacturaService.mongo.dto.Cliente;
+import com.prestafacturaService.mongo.repositories.propios.ClienteRepositoryCustom;
 
 
 @Repository
-public interface ClienteRepository extends GenericRepository<Cliente> {
+public interface ClienteRepository extends ClienteRepositoryCustom, GenericRepository<Cliente> {
 
 }
--- src/main/resources/com/prestafacturaService/mongo/repositories/ProvinciaRepository.java
+++ src/main/resources/com/prestafacturaService/mongo/repositories/ProvinciaRepository.java
@@ -3,9 +3,10 @@ package com.prestafacturaService.mongo.repositories;
 import org.springframework.stereotype.Repository;
 
 import com.prestafacturaService.mongo.dto.Provincia;
+import com.prestafacturaService.mongo.repositories.propios.ProvinciaRepositoryCustom;
 
 
 @Repository
-public interface ProvinciaRepository extends GenericRepository<Provincia>{
+public interface ProvinciaRepository extends ProvinciaRepositoryCustom, GenericRepository<Provincia>{
 
 }
