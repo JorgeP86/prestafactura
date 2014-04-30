9d8c8e5d172494b96e015835b7e13e5a335d6969
JorgeP86 <jorge.pareja86@gmail.com>
2014-04-30 19:37:12 +0200

    30/04/2014 añadido repositorios nuevos clientes, proveedores

--- src/main/resources/com/prestafacturaService/mongo/repositories/propios/ClienteRepositoryCustom.java
+++ src/main/resources/com/prestafacturaService/mongo/repositories/propios/ClienteRepositoryCustom.java
@@ -1,7 +1,11 @@
 package com.prestafacturaService.mongo.repositories.propios;
 
+import com.prestafacturaService.mongo.dto.Cliente;
+
 
 public interface ClienteRepositoryCustom {
 
 	public boolean existCliente(String identificacionFiscal);
+	public Cliente obtenerClienteById(Integer idCliente);
+	public Cliente updateCliente(Cliente clienteUpdate);
 }
--- src/main/resources/com/prestafacturaService/mongo/repositories/propios/ClienteRepositoryImpl.java
+++ src/main/resources/com/prestafacturaService/mongo/repositories/propios/ClienteRepositoryImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 
 import com.prestafacturaService.mongo.dto.Cliente;
 
@@ -17,6 +18,38 @@ public class ClienteRepositoryImpl implements ClienteRepositoryCustom {
 		Query query=new Query();
 		query.addCriteria(Criteria.where("identificacionFiscal").is(identificacionFiscal));
 		return mongoTemplate.exists(query, Cliente.class);
+	}
+
+	public Cliente obtenerClienteById(Integer idCliente) {
+		Query query=new Query();
+		query.addCriteria(Criteria.where("idCliente").is(idCliente));
+		// TODO Auto-generated method stub
+		return mongoTemplate.findOne(query, Cliente.class);
+	}
+
+	public Cliente updateCliente(Cliente clienteUpdate) {
+		// TODO Auto-generated method stub
+		Query query=new Query();
+		query.addCriteria(Criteria.where("idCliente").is(clienteUpdate.getIdCliente()));
+		Update update=new Update();
+		if(clienteUpdate.getNombre()!=null || !clienteUpdate.getNombre().trim().equals("")){
+			update.push("nombre", clienteUpdate.getNombre());
+			update.push("apellido1", clienteUpdate.getApellido1());
+			update.push("apellido2", clienteUpdate.getApellido2());
+			update.push("identificacionFiscal", clienteUpdate.getIdentificacionFiscal());
+			update.push("direccion", clienteUpdate.getDireccion());
+			update.push("dirEntrega", clienteUpdate.getDirEntrega());
+
+		}if(clienteUpdate.getDetallesContacto().getTelefono()!=null || !clienteUpdate.getDetallesContacto().getTelefono().trim().equals("")){
+			update.push("detallesContacto", clienteUpdate.getDetallesContacto());	
+		}if(clienteUpdate.getEntidadLegal().getNombreComercial()!=null || !clienteUpdate.getEntidadLegal().getNombreComercial().trim().equals("")){
+			update.push("entidadLegal", clienteUpdate.getEntidadLegal());
+			update.push("identificacionFiscal", clienteUpdate.getIdentificacionFiscal());
+			update.push("dirEntrega", clienteUpdate.getDirEntrega());
+			
+		}
+		return mongoTemplate.findAndModify(query, update, Cliente.class);
+		
 	}
 
 }
