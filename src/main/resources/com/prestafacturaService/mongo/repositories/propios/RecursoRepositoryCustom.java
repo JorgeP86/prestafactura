478e0848c9d55381c9a30620553d2bf5ecf7597b
JorgeP86 <jorge.pareja86@gmail.com>
2014-05-21 22:59:24 +0200

    21/05/2014- Modificarion altaRol debido al checkbox

--- src/main/resources/com/prestafacturaService/mongo/repositories/propios/RecursoRepositoryCustom.java
+++ src/main/resources/com/prestafacturaService/mongo/repositories/propios/RecursoRepositoryCustom.java
@@ -3,6 +3,6 @@ package com.prestafacturaService.mongo.repositories.propios;
 import com.prestafacturaService.mongo.dto.Recurso;
 
 public interface RecursoRepositoryCustom {
-	public Recurso obtenerPaginaByid(Integer idPagina);
+	public Recurso obtenerPaginaBypath(String path);
 
 }
